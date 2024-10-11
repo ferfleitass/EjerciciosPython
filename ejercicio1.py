bfaa23a6a12d827b4c98ae04ee6b8d6fe1e637d6
Fernando Fleitas <fleitasbaez55@gmail.com>
2024-10-11 13:28:40 -0300

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,60 +1,43 @@
-def ingresar_puntuacion_y_comentario():
+def main():
     while True:
-        print('Por favor, introduzca una puntuación en una escala de 1 a 5')
-        point = input()
+        print("Por favor, elige una opción:")
+        print("1. Introducir punto de valoración")
+        print("2. Ver resultados")
+        print("3. Salir")
         
-        if point.isdecimal():
-            point = int(point)
-            if point < 1 or point > 5:  # Verifica si es menor que 1 o mayor que 5
-                print('Por favor, introduzca un valor entre 1 y 5')
-            else:
-                print('Por favor, introduzca un comentario')
-                comment = input()
-                post = f'punto: {point} comentario: {comment}'
-                
-                with open("data.txt", 'a') as file_pc:
-                    file_pc.write(f'{post}\n')
-                break
-        else:
-            print('Por favor, introduzca la puntuación en números')
-
-
-def comprobar_resultados():
-    print('Resultados hasta la fecha:')
-    try:
-        with open("data.txt", "r") as read_file:
-            content = read_file.read()
-            if content:
-                print(content)
-            else:
-                print('No hay resultados registrados.')
-    except FileNotFoundError:
-        print('No se ha encontrado el archivo de resultados.')
-
-
-def seleccionar_proceso():
-    while True:
-        print('Seleccione el proceso que desea aplicar:')
-        print('1: Ingresar puntuación y comentario')
-        print('2: Comprueba los resultados obtenidos hasta ahora.')
-        print('3: Finalizar')
-        num = input()
-        
-        if num.isdecimal():
-            num = int(num)
+        try:
+            num = int(input("Introduce un número del 1 al 3: "))
             
             if num == 1:
-                ingresar_puntuacion_y_comentario()  
+                introducir_valoracion()
             elif num == 2:
-                comprobar_resultados() 
+                comprobar_resultados()
             elif num == 3:
-                print('Finalizando')
-                break  
+                print('Saliendo del programa...')
+                break
             else:
-                print('Introduzca un número del 1 al 3')  
-        else:
-            print('Introduzca un número del 1 al 3')  
+                print('Por favor, introduce un número del 1 al 3.')
+        except ValueError:
+            print('Eso no es un número válido. Intenta de nuevo.')
 
+def introducir_valoracion():
+    while True:
+        try:
+            valoracion = int(input("¿Cuál es tu valoración del 1 al 5? "))
+            if 1 <= valoracion <= 5:
+                comentario = input("Deja un comentario: ")
+                # Aquí podrías guardar la valoración y el comentario
+                print(f'Valoración registrada: {valoracion}, Comentario: "{comentario}"')
+                break
+            else:
+                print('Introduce un número entre 1 y 5, por favor.')
+        except ValueError:
+            print('Eso no es un número. Intenta nuevamente.')
 
+def comprobar_resultados():
+    # Aquí podrías mostrar los resultados almacenados
+    print("Aquí irían los resultados guardados.")
+    # Ejemplo: print(resultados)
 
-seleccionar_proceso()
+if __name__ == "__main__":
+    main()
